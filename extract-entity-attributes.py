b4b5c114d5843102e834ff52a16591fe29a74b2c
Michael Tsang <michaeltsang1998@gmail.com>
2018-10-14 13:41:57 +0100

    Add basic phrase extraction for single attribute with single entity based on dependencies

--- extract-entity-attributes.py
+++ extract-entity-attributes.py
@@ -2,6 +2,8 @@
 
 import argparse
 import spacy
+from spacy import displacy
+from spacy.symbols import acomp, dobj, nsubj, VERB
 
 
 def add_to_dict(dictionary, key, value):
@@ -11,53 +13,52 @@ def add_to_dict(dictionary, key, value):
         dictionary[key] = [value]
 
 
+def get_phrase(token):
+    subtree = token.subtree
+    return " ".join(map(lambda x: x.text, subtree))
+
+
+def extract_attribute(subjects, attributes, token):
+    for child in token.children:
+        dep = child.dep
+        if dep == acomp or dep == dobj:
+            attributes.append(token.text + " " + get_phrase(child))
+        elif dep == nsubj:
+            subjects.append(child)
+
+
 def main(text):
-    VERBOSE = False
-    SKIP_ENTITIES = ['LANGUAGE', 'DATE', 'TIME', 'PERCENT', 'MONEY', 'QUANTITY', 'ORDINAL', 'CARDINAL']
-    ADJ_JOIN_POS = ['ADJ', 'ADV', 'CCONJ', 'ADP']
-
     print('Loading model.')
-
-    nlp = spacy.load('en_core_web_lg')
+    nlp = spacy.load('en')
 
     print('Processing text.')
-
     doc = nlp(text)
 
     print('Extracting entity-attribute pairs.')
 
     dictionary = {}
 
-    for entity in doc.ents:
-        if entity.label_ in SKIP_ENTITIES:
-            continue
+    verbs = set()
 
-        if VERBOSE:
-            print('-- Found entity:', entity.text + '(' + entity.lemma_ + ')', entity.label_)
+    for token in doc:
+        if token.pos == VERB and token not in verbs:
+            verbs.add(token)
+            subjects = list()
+            attributes = list()
 
-        attr = None
+            extract_attribute(subjects, attributes, token)
 
-        for i in range(len(entity.sent), 0, -1):
-            token = entity.sent[i - 1]
+            for subject in subjects:
+                for attribute in attributes:
+                    add_to_dict(dictionary, subject, attribute)
 
-            if attr is not None:
-                if token.pos_ in ADJ_JOIN_POS:
-                    attr = token.lemma_ + ' ' + attr
-                else:
-                    add_to_dict(dictionary, entity.lemma_, attr)
-                    attr = None
-            elif token.pos_ == 'ADJ':
-                attr = token.lemma_
+    print(dictionary)
 
-            if VERBOSE:
-                print('- Found token:', token.text + '(' + token.lemma_ + ')', token.pos_,
-                      token.dep_ + '(' + spacy.explain(token.tag_) + ')')
-
-    return dictionary
+    displacy.serve(doc, style="dep")
 
 
 text = '''Python packaging may or may not actually be very bad but at the same time also great today.''' \
-       ''' Google's directions are not very much as terrible or bad as you think.'''
+       '''Google's directions are not very much as terrible or bad as you think.'''
 # Outputs {'python': ['also great', 'same', 'very bad'], 'google': ['not very much as terrible or bad']}
 
 if __name__ == "__main__":
@@ -73,4 +74,5 @@ if __name__ == "__main__":
     else:
         data = args.text
 
-    print(main(data))
+    # print(main(data))
+    main(data)
