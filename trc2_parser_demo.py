718d2dc133a22f3c9abe4fb87ba079cfdf7b6f1b
Ian Yeo <iy116@ic.ac.uk>
2018-10-14 16:08:02 +0100

    Fix invalid import.

--- trc2_parser_demo.py
+++ trc2_parser_demo.py
@@ -1,12 +1,12 @@
-import parse_trc2
+import trc2_parser
 
-name = 'data/1.txt'
+name = 'data/trc2.csv'
 file = open(name)
 
-parse_trc2.prepare(file)
+trc2_parser.prepare(file)
 
 for _ in range(100):
-    line = parse_trc2.next(file, True)
+    line = trc2_parser.next(file, True)
 
     if line is None:
         break
