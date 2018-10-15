1e9b8c04c341954404075a2e2b313ee9e990beca
Wenquan Zou <wz1816@ic.ac.uk>
2018-10-15 16:47:17 +0100

    [wz1816] create framework function to build with todo

---
+++ src/train.py
@@ -0,0 +1,13 @@
+
+def create_training_data():
+    pass
+
+def start_training():
+    pass
+
+def create_model():
+    pass
+
+def evalutation(model, data):
+    # return all the E&A pairs and evidences
+    pass
