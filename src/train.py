46fb22584c606759f0eb46a6b8c22bdb4e9a4da1
Wenquan Zou <wz1816@ic.ac.uk>
2018-10-15 17:11:08 +0100

    [wz1816][oft16][mct16][iy116] Implement train in creating model and parser instance.

--- src/train.py
+++ src/train.py
@@ -1,12 +1,24 @@
+import spacy
+
 
 def create_training_data():
     pass
 
-def start_training():
-    pass
 
-def create_model():
-    pass
+def start_training(model=None, output=None, epoch=10):
+    # Loading or create a empty model.
+    if model is not None:
+        nlp = spacy.load(model)
+        print("Loaded model '%s'." % model)
+    else:
+        nlp = spacy.blank('en')
+        print("Create blank model to train.")
+
+    # Create a fresh instance of parser.
+    if 'parser' in nlp.pipe_names:
+        nlp.remove_pipe('parser')
+    nlp.add_pipe(nlp.create_pipe('parser'), first=True)
+
 
 def evalutation(model, data):
     # return all the E&A pairs and evidences
