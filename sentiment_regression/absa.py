d19644e34287b0c8d1637fff1cd08608a188f3e1
Anson Miu <ansonmiu0214@gmail.com>
2018-10-30 20:32:07 +0000

    [kcm116][oft16][wz1816] Implemented basic pipeline to get sentiment score for entity from document

---
+++ sentiment_regression/absa.py
@@ -0,0 +1,36 @@
+from argparse import ArgumentParser
+import extractor
+import bag_of_words
+
+default_text = "The iPhone has a great camera but a poor screen. The MacBook has excellent battery, just like the iPhone. The iPhone and MacBook also have terrible prices."
+default_entity = "iPhone"
+
+if __name__ == "__main__":
+  parser = ArgumentParser()
+  parser.add_argument('-d', '--document')
+  parser.add_argument('-t', '--text', default=default_text)
+  parser.add_argument('-e', '--entity', default=default_entity)
+  parser.add_argument('-f', '--training')
+
+  args = parser.parse_args()
+
+  data = ''
+  if args.document:
+    with open(args.document, 'r') as f:
+      data = f.read().replace('\n', '')
+  else:
+    data = args.text
+  
+  eas = extractor.main(data, args.entity)
+  print("Relevant aspects: {}".format(eas))
+
+  model = bag_of_words.train_model(args.training)
+
+  aggregation = []
+
+  for entry in eas:
+    entity, attribute, sentiment = entry
+    [score] = bag_of_words.predict(model, sentiment)
+    aggregation.append((entry, score))
+  
+  print(aggregation)
