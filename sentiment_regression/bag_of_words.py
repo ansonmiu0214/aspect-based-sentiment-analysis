7054525879ca9bdc87920dd53c79d11e5ebab428
Anson Miu <ansonmiu0214@gmail.com>
2018-10-30 20:30:18 +0000

    [kcm116][oft16][wz1816] Delegated model/prediction logic from main function to individual functions

--- sentiment_regression/bag_of_words.py
+++ sentiment_regression/bag_of_words.py
@@ -1,11 +1,15 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
+from argparse import ArgumentParser
+from pathlib import Path
+
+polynomial_degree = 2
 
 sample_docs = [
   "The iPhone has a great camera.", 
   "The screen on the iPhone is not great.",
-  "The iPhone provides great battery life for users."
+  "The iPhone provides great battery life for users.",
   "The iPhone's camera isn't great"
 ]
 
@@ -33,16 +37,47 @@ def generate_matrix(vectorizer=vectorizer, docs=sample_docs, sentiments=sample_s
   return matrix, sentiments
 
 
-if __name__ == "__main__":
+def train_model(folder, extension='txt'):
+  pathlist = Path(folder).glob('*.' + extension)
+  docs = []
+  sentiments = []
+  for path in pathlist:
+    print(path)
+    with open(path, 'r') as f:
+      for line in f:
+        line = line.strip()
+        
+        split = len(line) - 1
+        sentence, score = line[:split], line[split:]
+        docs.append(sentence.lower())
+        sentiments.append(-1 if int(score) == 0 else 1) 
 
-  matrix, vector = generate_matrix()
-  print(matrix.toarray())
-  print(vector)
+  matrix, vector = generate_matrix(vectorizer, docs, sentiments)
   model = LinearRegression()
-  model.fit(matrix,vector)
-  test = "It is not the case that my day is not great"
-  test_vector =vectorizer.transform([test])
-  print(model.predict(test_vector))
-  print('Done!')
+  
+  print("Fitting model")
+  model.fit(matrix, vector)
+  return model
+
+def predict(model, text):
+  test_vector = vectorizer.transform([text])
+  return model.predict(test_vector)
 
 
+if __name__ == "__main__":
+  parser = ArgumentParser()
+  parser.add_argument('folder', type=str)
+
+  args = parser.parse_args()
+  folder = args.folder
+
+  model = train_model(folder)
+  
+  while True:
+    print("Enter test text: ", end="")
+    text = input().strip()
+    if not text:
+      break
+
+    score = predict(model, text)
+    print(score)
