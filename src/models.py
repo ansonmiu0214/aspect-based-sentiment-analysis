af412ad3b9aa35c9385eb0c96d3b00fb6550e238
Anson Miu <ansonmiu0214@gmail.com>
2018-11-06 15:44:37 +0000

    [kcm116] Unified template ABCs into a single `models.py` for clarity.

--- src/models.py
+++ src/models.py
@@ -1,7 +1,42 @@
+import abc
+from typing import List
+
+
+##############
+# Models
+##############
+
+class HasMetadata(abc.ABC):
+    def __init__(self):
+        self.metadata = dict()
+
+    def add_metadata(self, key, value):
+        self.metadata[key] = value
+
+
+class Document(HasMetadata):
+    def __init__(self):
+        super().__init__()
+        self.components = []  # type: List[DocumentComponent]
+        self.entities = []  # type: List[EntityEntry]
+
+    def add_component(self, component):
+        self.components.append(component)
+
+    def add_entity(self, entity):
+        self.entities.append(entity)
+
+
+class DocumentComponent:
+    def __init__(self, type, text):
+        self.type = type  # type: str
+        self.text = text  # type: str
+
+
 class EntityEntry:
     def __init__(self, name):
         self.name = name
-        self.attributes = []
+        self.attributes = []  # type: List[AttributeEntry]
 
     def add_attribute(self, attribute):
         '''
@@ -11,14 +46,103 @@ class EntityEntry:
         self.attributes.append(attribute)
 
 
-class AttributeEntry:
-    def __init__(self, attribute, expression):
-        self.attribute = attribute
-        self.expression = expression
-        self.metadata = dict()
-
-    def add_metadata(self, key, value):
-        self.metadata[key] = value
+class AttributeEntry(HasMetadata):
+    def __init__(self, attribute, expression, sentiment=None):
+        super().__init__()
+        self.attribute = attribute  # type: str
+        self.expression = expression  # type: str
+        self.sentiment = sentiment  # type: float
 
     def __repr__(self):
-        return "attr={} expr={}".format(self.attribute, self.expression)
+        truncate = lambda n, x: x[:n] + "..."
+        max_length = 10
+        return "Attr={} Exprs={} Sent={}".format(self.attribute,
+                                                 list(map(lambda x: truncate(max_length, x), self.expression)),
+                                                 self.sentiment)
+
+
+class Query:
+    def __init__(self, entity, attribute, positive_sentiment):
+        self.entity = entity
+        self.attribute = attribute
+        self.positive_sentiment = positive_sentiment
+
+
+##############
+# Strategies
+##############
+
+class PreprocessorService(abc.ABC):
+    @abc.abstractmethod
+    def preprocess(self, doc):
+        '''
+
+        :param doc:
+        :rtype: Document
+        '''
+        pass
+
+
+class ExtractorService(abc.ABC):
+    @abc.abstractmethod
+    def extract(self, doc: Document) -> Document:
+        '''
+
+        :param doc:
+        :rtype: Document
+        '''
+        pass
+
+
+class SentimentService(abc.ABC):
+    @abc.abstractmethod
+    def compute_sentiment(self, text):
+        '''
+
+        :param text: str
+        :rtype: float
+        '''
+        pass
+
+
+class QueryParser(abc.ABC):
+    @abc.abstractmethod
+    def parse_query(self, text) -> Query:
+        '''
+
+
+        :param text:
+        :rtype: Query
+        '''
+        pass
+
+
+class DataSourceService(abc.ABC):
+    @abc.abstractmethod
+    def process_document(self, document: Document):
+        '''
+
+        :param document:
+        :return:
+        '''
+        pass
+
+    @abc.abstractmethod
+    def lookup(self, query: Query) -> List[AttributeEntry]:
+        '''
+
+        :param query:
+        :return:
+        '''
+        pass
+
+
+class AggregatorService(abc.ABC):
+    @abc.abstractmethod
+    def aggregate_sentiment(self, sentiments):
+        '''
+
+        :param sentiments: [float]
+        :rtype: float
+        '''
+        pass
