904efc3c9604b10ae18ba443e74da15e7fa52182
Michael Tsang <michaeltsang1998@gmail.com>
2018-11-09 17:56:39 +0000

    Parse xml to get paragraphs contained with text tags

--- src/preprocessor_service/text_preprocessor.py
+++ src/preprocessor_service/text_preprocessor.py
@@ -1,18 +1,31 @@
+import xml.etree.ElementTree as ET
 from src.models import DocumentComponent, PreprocessorService, Document
-
 
 class TextPreprocessor(PreprocessorService):
     '''
     Simple input preprocessor, takes string input and wraps
     '''
 
-    def preprocess(self, doc):
+    def preprocess(self, xml):
         document = Document()
 
         document.add_metadata("title", "Imported from TextPreprocessor")
 
-        for paragraph in doc.strip().split('\n\n'):
-            component = DocumentComponent('text', paragraph)
-            document.add_component(component)
+        tree = ET.parse(xml)
+        root = tree.getroot()
+
+        content = ' '.join(map(lambda i: i.text, root.findall('./text/p')))
+        document.add_component(DocumentComponent('content', content))
 
         return document
+
+
+def main():
+    tp = TextPreprocessor()
+    with open('../444498newsML.xml') as file:
+        document = tp.preprocess(file)
+
+
+
+if __name__ == '__main__':
+    main()
