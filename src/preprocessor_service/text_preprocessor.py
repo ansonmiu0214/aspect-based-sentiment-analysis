b797e8872e0652355676bed3d411b0ac9d0956d3
Michael Tsang <michaeltsang1998@gmail.com>
2018-11-09 18:13:03 +0000

    Add author and headline data to document

--- src/preprocessor_service/text_preprocessor.py
+++ src/preprocessor_service/text_preprocessor.py
@@ -6,16 +6,26 @@ class TextPreprocessor(PreprocessorService):
     Simple input preprocessor, takes string input and wraps
     '''
 
+    @staticmethod
+    def concat_elements(root, tag):
+        return ' '.join(map(lambda i: i.text, root.findall(tag)))
+
     def preprocess(self, xml):
         document = Document()
 
-        document.add_metadata("title", "Imported from TextPreprocessor")
+        document.add_metadata('title', 'Imported from TextPreprocessor')
 
         tree = ET.parse(xml)
         root = tree.getroot()
 
-        content = ' '.join(map(lambda i: i.text, root.findall('./text/p')))
+        content = self.concat_elements(root, './text/p')
         document.add_component(DocumentComponent('content', content))
+
+        headline = self.concat_elements(root, './headline')
+        document.add_component(DocumentComponent('headline', headline))
+
+        author = self.concat_elements(root, './byline')
+        document.add_metadata('author', author)
 
         return document
 
