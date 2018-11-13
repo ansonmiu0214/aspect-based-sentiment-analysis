0b9a640015d5075e9db24708742feb787d8be886
Wenquan Zou <wz1816@ic.ac.uk>
2018-11-13 17:56:40 +0000

    [wz1816]Preprocess data without analysis

--- src/extractor_service/data_clean.py
+++ src/extractor_service/data_clean.py
@@ -1,13 +1,32 @@
+import pandas as pd
 import spacy
+import random
+from collections import Counter
+
+
+# Define function to cleanup text by removing personal pronouns, stopwords, and puncuation
+def cleanup_text(docs, logging=True):
+    nlp = spacy.load('en')
+    texts = []
+    counter = 1
+    for doc in docs:
+        if counter % 1000 == 0 and logging:
+            print("Processed %d out of %d documents." % (counter, len(docs)))
+        counter += 1
+        doc = nlp(doc, disable=['parser', 'ner'])
+        tokens = [word for word in doc if not word.is_stop and not word.is_punct]
+        tokens = [tok.lemma_.lower().strip() for tok in doc if tok.lemma_ != '-PRON-']
+        tokens = ' '.join(tokens)
+        texts.append(tokens)
+    return pd.Series(texts)
 
 
 def data_clean(path):
     file = open(path)
-    nlp = spacy.load('en')
-    docx = nlp(file.readlines())
-    data_cleaned = [word for word in docx if not word.is_stop and not word.is_punct]
-    print(data_cleaned)
+    data = file.readlines()
+    print(cleanup_text(data))
+
 
 
 if __name__ == '__main__':
-    data_clean("test.ft.txt")
+    data_clean("train.ft.txt")
