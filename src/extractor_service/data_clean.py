517fadfc5e4261e2ad2f522e8b8eb4b22a9dc5db
Wenquan Zou <wz1816@ic.ac.uk>
2018-11-14 11:38:45 +0000

    [wz1816]Change the dataset from product to movie

--- src/extractor_service/data_clean.py
+++ src/extractor_service/data_clean.py
@@ -1,3 +1,5 @@
+import thinc.extra.datasets
+
 import pandas as pd
 import spacy
 import random
@@ -5,6 +7,12 @@ from collections import Counter
 
 
 # Define function to cleanup text by removing personal pronouns, stopwords, and puncuation
+from spacy.symbols import NOUN
+from spacy.lang.en.stop_words import STOP_WORDS
+
+stopwords = list(STOP_WORDS)
+
+
 def cleanup_text(docs, logging=True):
     nlp = spacy.load('en')
     texts = []
@@ -14,20 +22,23 @@ def cleanup_text(docs, logging=True):
             print("Processed %d out of %d documents." % (counter, len(docs)))
         counter += 1
         doc = nlp(doc, disable=['parser', 'ner'])
-        tokens = [word for word in doc if not word.is_stop and not word.is_punct]
-        tokens = [tok.lemma_.lower().strip() for tok in doc if tok.lemma_ != '-PRON-']
+        tokens = [tok.lemma_.lower().strip() for tok in doc if tok.pos == NOUN and not tok.is_stop]
         tokens = ' '.join(tokens)
         texts.append(tokens)
     return pd.Series(texts)
 
 
-def data_clean(path):
-    file = open(path)
-    data = file.readlines()
-    random.shuffle(data)
-    print(cleanup_text(data[:50000]))
-
+def data_clean():
+    train_data, _ = thinc.extra.datasets.imdb()
+    random.shuffle(train_data)
+    train_data = [i[0] for i in train_data]
+    text_cleaned = cleanup_text(train_data)
+    text_cleaned = ' '.join(text_cleaned).split()
+    text_cleaned = [word for word in text_cleaned if word != '\'s']
+    text_counter = Counter(text_cleaned)
+    most_common = [word[0] for word in text_counter.most_common(10)]
+    print(most_common)
 
 
 if __name__ == '__main__':
-    data_clean("train.ft.txt")
+    data_clean()
