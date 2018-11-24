1f355461280fcc1c9077e89a9ab8dbd64da276c6
Seoin Chai <sc5316@ic.ac.uk>
2018-11-24 14:46:49 +0000

    Separated eas database utils into a separate python file, refactored aws database

--- src/database/aws_database.py
+++ src/database/aws_database.py
@@ -1,7 +1,6 @@
 import pymysql
 import time
 import datetime
-from eas import Eas
 
 host = "myspicyanalysis.chywzz3l5nmu.us-east-1.rds.amazonaws.com"
 port = 3306
@@ -9,48 +8,44 @@ dbname = "dbspicyanalysis"
 user = "seoinchai"
 password = "spicyanalysis"
 
-try:
-    connection = pymysql.connect(host, user=user, port=port, passwd=password, db=dbname)
 
-except Exception as e:
-    print(e)
+def get_connection():
+    try:
+        connection = pymysql.connect(host, user=user, port=port, passwd=password, db=dbname)
+        return connection
 
-c = connection.cursor()
-
-c.execute("""DROP TABLE eas_pair""")
-
-c.execute("""CREATE TABLE eas_pair(
-                        entity VARCHAR(50),
-                        attribute VARCHAR(50),
-                        sentiment VARCHAR(50),
-                        sentence VARCHAR(200),
-                        document VARCHAR(1000),
-                        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP)""")
+    except Exception as e:
+        print(e)
 
 
-def insert_eas(eas):
-    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-    with connection:
-        c.execute("""INSERT INTO eas_pair VALUES ('%s', '%s', '%s', '%s', '%s', '%s')""" %
-                  (eas.entity,
-                   eas.attribute,
-                   eas.sentiment,
-                   eas.sentence,
-                   eas.document,
-                   timestamp))
+def get_cursor(connection):
+    return connection.cursor()
 
 
-def get_eas_by_entity(entity):
-    c.execute("""SELECT * FROM eas_pair WHERE entity='%s'""" % entity)
-    return c.fetchall()
+def drop_table(cursor, table_name):
+    cursor.execute("DROP TABLE " + table_name)
 
 
-eas1 = Eas("Iphone", "camera", "good", "Iphone has a good camera", "document1")
-eas2 = Eas("Iphone", "screen", "bad", "Iphone has a bad screen", "document1")
+# Takes in the table name and attributes dictionary
+def create_table(cursor, table_name, attributes):
+    attributes_format = ""
+    for key in attributes.keys():
+        attributes_format = attributes_format + key + " " + attributes[key] + ", "
+    attributes_format = attributes_format[:-2]
 
-insert_eas(eas1)
-insert_eas(eas2)
+    cursor.execute("CREATE TABLE " + table_name + "(" + attributes_format + ")")
 
-print(get_eas_by_entity("Iphone"))
+    # --for EAS--
+    # entity TEXT,
+    # attribute TEXT,
+    # sentiment TEXT,
+    # sentence TEXT,
+    # document TEXT,
+    # timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP)""")
 
-connection.close()
+
+
+
+
+def close_connection(connection):
+    connection.close()
