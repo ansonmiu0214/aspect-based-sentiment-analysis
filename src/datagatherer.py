08df4a512ba0febe8c33875f4916bdd7aabd1ef0
Michael Tsang <michaeltsang1998@gmail.com>
2018-12-21 14:32:17 +0000

    Add labelling in data gatherer

--- src/datagatherer.py
+++ src/datagatherer.py
@@ -1,5 +1,6 @@
 import csv
 import sys
+import string
 from pprint import pprint
 
 from aggregator_service.average_aggregator import AverageAggregator
@@ -13,6 +14,8 @@ from query_parser.simple_parser import SimpleParser
 from sentiment_service.vader import Vader
 from main import ABSA
 
+TRANSLATOR = str.maketrans('', '', string.punctuation)
+
 def extract_all_tuples(doc):
     all_tuples = list()
     for ent in doc.entities:
@@ -23,14 +26,69 @@ def extract_all_tuples(doc):
                             'entity': ent.name,
                             'attribute': attr.attribute,
                             'expression': exp
-                        })
+                        }
+                )
     return all_tuples
 
+
+def find_sublist(s, l):
+    start = -1
+    matched = 0
+    for (i, word) in enumerate(l):
+        if matched == len(s):
+            break
+
+        if s[matched] == word:
+            matched += 1
+            if start == -1:
+                start = i
+        else:
+            matched = 0
+            start = -1
+
+    if matched == len(s):
+        return start
+    return -1
+
+def format_for_matching(s):
+    return s.lower().translate(TRANSLATOR).split()
+
 def label(t):
-    pass
+    ent = format_for_matching(t['entity'])
+    attr = format_for_matching(t['attribute'])
+    exp = t['expression']
+    sep = format_for_matching(exp)
+    length = len(sep)
+    heads = [0] * length
+    deps = ['-'] * length
+
+    ent_start = find_sublist(ent, sep)
+
+    # Ignore coreferencing instances
+    if ent_start == -1:
+        return None
+
+    heads[ent_start] = ent_start
+    deps[ent_start] = 'ENTITY'
+    for i in range(ent_start + 1, ent_start + len(ent)):
+        heads[i] = i - 1
+        deps[i] = 'ENTITY_ADD'
+
+    attr_start = find_sublist(attr, sep)
+    # Allow possibility of no attribute
+    if attr_start != -1:
+        heads[attr_start] = ent_start
+        deps[attr_start] = 'ATTRIBUTE'
+        for i in range(attr_start + 1, attr_start + len(attr)):
+            heads[i] = i - 1
+            deps[i] = 'ATTRIBUTE_ADD'
+
+    return {'expression': exp, 'heads': heads, 'deps' : deps}
+
 
 def label_all_tuples(tuples):
     return map(label, tuples)
+
 
 def main(sourcefile):
     sentiment_service = Vader()
@@ -45,17 +103,20 @@ def main(sourcefile):
     with open(sourcefile) as file:
         doc = absa.load_document(file)
 
-    print(extract_all_tuples(doc))
+    all_tuples = extract_all_tuples(doc)
+    labelled = label_all_tuples(all_tuples)
+
+    # Remove coreferencing artifacts
+    labelled = filter(lambda x: x is not None, labelled)
 
 
     with open('training.csv', 'w', newline='') as csvfile:
-        fieldnames = ['first_name', 'last_name']
+        fieldnames = ['expression', 'heads', 'deps']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         writer.writeheader()
-        writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
-        writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
-        writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
+        for data in labelled:
+            writer.writerow(data)
 
 if __name__ == '__main__':
     print(sys.argv[1])
