bdfe5963c3149868e66309780aebcda3fe48b215
Michael Tsang <michaeltsang1998@gmail.com>
2019-01-10 23:15:46 +0000

    Add comment to aggregator

--- src/aggregator_service/average_aggregator.py
+++ src/aggregator_service/average_aggregator.py
@@ -2,10 +2,17 @@ from models import AggregatorService
 
 
 class AverageAggregator(AggregatorService):
+    HEADLINE_MULTIPLIER = 2
+    CONTENT_MULTIPLIER = 1
+
     def aggregate_sentiment(self, data):
+        '''
+        Calculates the weighted average of the given sentiment.
+
+        :param data: [{'sentiment': float, 'is_header': bool}]
+        :rtype: float
+        '''
         sum = 0
-        HEADLINE_MULTIPLIER = 2
-        CONTENT_MULTIPLIER = 1
 
         for row in data:
             sum += row['sentiment'] * (HEADLINE_MULTIPLIER if row['is_header'] else CONTENT_MULTIPLIER)
