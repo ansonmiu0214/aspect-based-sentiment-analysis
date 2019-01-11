e42a8e6516192e24f61a153e47a1eb69401e2575
Anson Miu <ansonmiu0214@gmail.com>
2019-01-11 12:09:50 +0000

    [kcm116] Migrated BoW and word vectors to research directory

--- research/wordvectors/wordvector_demo.py
+++ research/wordvectors/wordvector_demo.py
@@ -4,21 +4,22 @@ import numpy as np
 import word2vec
 
 
-"""
-TODO handle KeyError for `unknown' words
-"""
 def word_vector_of_sentence(sentence):
-  matrix = np.asmatrix([model[word.lower()] for word in sentence])
-  return matrix
+    matrix = np.asmatrix([model[word.lower()] for word in sentence])
+    return matrix
+
 
 if __name__ == "__main__":
-  model = word2vec.load('model.bin')
-  print("Loaded model.")
+    model = word2vec.load('model.bin')
+    print("Loaded model.")
 
-  print("Enter sentence: ", end="")
-  sentence = input().strip().split()
+    print("Enter sentence: ", end="")
+    sentence = input().strip().split()
 
-  matrix = word_vector_of_sentence(sentence)
-  print(type(matrix))
-  print(matrix.shape)
-  print(matrix)
+    try:
+        matrix = word_vector_of_sentence(sentence)
+        print(type(matrix))
+        print(matrix.shape)
+        print(matrix)
+    except KeyError:
+        print("Unknown word(s) '%s'" % sentence)
